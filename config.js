729623d16e3c68d15b9d969cb1ce27dd59eee841
vanhien8626 <129392400+vanhien8626@users.noreply.github.com>
2023-04-03 21:20:41 +0700

    Upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Tớ Muốn Hỏi Cậu Một Câu Hỏi <3`,
+    btnIntro: 'hihi',
+    title: 'Mình rất yêu bạn,bạn có chịu làm ny mik lần nữa hông',
+    desc: 'Thoát ra là chịu làm ny mik r nha ',
+    btnYes: 'Ok,lun nè<33',
+    btnNo: 'Mơ đi cưng:3',
+    question:'Lí do cậu yêu tớ đi :vvvv <3',
+    btnReply: 'Gửi cho mày<3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Tao biết mà 🥰. Yêu Mày nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/VanHien8606' //link mess của các bạn. VD: 
+}
